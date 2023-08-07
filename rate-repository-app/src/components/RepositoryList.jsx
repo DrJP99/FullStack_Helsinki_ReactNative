b38d507a2b6629001cc5fc287901c7e0068dcf06
Juan Pablo Gonzalez <43384928+DrJP99@users.noreply.github.com>
2023-08-07 15:38:24 -0600

    10.23

--- rate-repository-app/src/components/RepositoryList.jsx
+++ rate-repository-app/src/components/RepositoryList.jsx
@@ -2,6 +2,8 @@ import { FlatList, View, StyleSheet } from 'react-native';
 import RepositoryItem from './RepositoryItem';
 import theme from '../theme';
 import useRepositories from '../hooks/useRepositories';
+import { useEffect, useState } from 'react';
+import { Picker } from '@react-native-picker/picker';
 
 const styles = StyleSheet.create({
 	separator: {
@@ -12,16 +14,70 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
+const SelectOption = ({ setOrderDirection, setOrderBy }) => {
+	const [selected, setSelected] = useState('latest');
+
+	const handleSelect = (value) => {
+		setSelected(value);
+
+		switch (value) {
+			case 'latest':
+				setOrderDirection('DESC');
+				setOrderBy('CREATED_AT');
+				break;
+			case 'highest':
+				setOrderDirection('DESC');
+				setOrderBy('RATING_AVERAGE');
+				break;
+			case 'lowest':
+				setOrderDirection('ASC');
+				setOrderBy('RATING_AVERAGE');
+				break;
+			default:
+				setOrderDirection('DESC');
+				setOrderBy('CREATED_AT');
+				break;
+		}
+	};
+
+	return (
+		<Picker
+			selectedValue={selected}
+			onValueChange={(itemValue) => {
+				handleSelect(itemValue);
+			}}
+		>
+			<Picker.Item label='Latest repositories' value={'latest'} />
+			<Picker.Item label='Highest rated repositories' value={'highest'} />
+			<Picker.Item label='Loswest rated repositories' value={'lowest'} />
+		</Picker>
+	);
+};
+
 const RepositoryList = () => {
-	const { repositories } = useRepositories();
+	// const [orderDirection, setOrderDirection] = useState('DESC');
+	// const [orderBy, setOrderBy] = useState('CREATED_AT');
+
+	const { repositories, refetch, setOrderDirection, setOrderBy } =
+		useRepositories();
 
 	const repositoryNodes = repositories
 		? repositories.edges.map((edge) => edge.node)
 		: [];
 
+	// useEffect(() => {
+	// 	setOrder(orderDirection, orderBy);
+	// }, [orderBy]);
+
 	return (
 		<FlatList
 			data={repositoryNodes}
+			ListHeaderComponent={
+				<SelectOption
+					setOrderDirection={setOrderDirection}
+					setOrderBy={setOrderBy}
+				/>
+			}
 			ItemSeparatorComponent={ItemSeparator}
 			renderItem={({ item }) => <RepositoryItem item={item} />}
 			keyExtractor={(item) => item.id}
--- rate-repository-app/src/hooks/useRepositories.jsx
+++ rate-repository-app/src/hooks/useRepositories.jsx
@@ -1,41 +1,33 @@
 import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
+import { useEffect, useState } from 'react';
 
 const useRepositories = () => {
-	// const [repositories, setRespositories] = useState();
-	// const [loading, setLoading] = useState(false);
+	const [orderDirection, setOrderDirection] = useState('DESC');
+	const [orderBy, setOrderBy] = useState('CREATED_AT');
 
 	const { data, error, loading, refetch } = useQuery(GET_REPOSITORIES, {
+		variables: {
+			orderDirection,
+			orderBy,
+		},
 		fetchPolicy: 'cache-and-network',
+		enabled: false,
 	});
+
 	let repositories = undefined;
 	if (!loading && !error) {
 		repositories = data.repositories;
 	}
 
-	// useEffect(() => {
-	// 	if (!loading && !error) {
-	// 		setRespositories(data);
-	// 	} else {
-	// 		setRespositories(undefined);
-	// 	}
-	// }, [loading]);
-
-	// const fetchRepositories = async () => {
-	// 	setLoading(true);
-
-	// 	const res = await fetch('http://192.168.10.133:5000/api/repositories');
-	// 	const json = await res.json();
-
-	// 	setLoading(false);
-	// 	setRespositories(json);
-	// };
-
-	// useEffect(() => {
-	// 	fetchRepositories();
-	// }, []);
-
-	return { repositories, loading, error, refetch };
+	return {
+		repositories,
+		loading,
+		error,
+		refetch,
+		setOrderDirection,
+		setOrderBy,
+	};
 };
 
 export default useRepositories;
